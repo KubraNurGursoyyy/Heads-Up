a9777213449df18f51cbda9e1e4240fbe136f776
kubranur.gursoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2026-08-28 16:20:10 +0300

    Fix NestJS auth module dependencies

--- apps/api/src/watches/watches.module.ts
+++ apps/api/src/watches/watches.module.ts
@@ -1,7 +1,18 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { WatchesController } from './watches.controller';
-import { WatchesService } from './watches.service';
+import { AuthModule } from '../auth/auth.module';
 import { AiModule } from '../ai/ai.module';
 import { JobsModule } from '../jobs/jobs.module';
-@Module({ imports:[AiModule, forwardRef(()=>JobsModule)], controllers:[WatchesController], providers:[WatchesService], exports:[WatchesService] })
+import { WatchesController } from './watches.controller';
+import { WatchesService } from './watches.service';
+
+@Module({
+  imports: [
+    AuthModule,
+    AiModule,
+    forwardRef(() => JobsModule),
+  ],
+  controllers: [WatchesController],
+  providers: [WatchesService],
+  exports: [WatchesService],
+})
 export class WatchesModule {}
