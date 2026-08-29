d6f3413f8ab00ebb47675896baa54d01c9af7116
kubranur.gursoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2026-08-29 18:19:56 +0300

    app.config

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -4,6 +4,7 @@ module.exports = {
   expo: {
     name: 'HeadsUp',
     slug: 'headsup',
+    owner: 'kubranurgursoyys-team',
     version: '0.1.0',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
