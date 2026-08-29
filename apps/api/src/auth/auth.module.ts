6f2cd53e40fcbc47bc51c3de02f8c955a70fd1c7
kubranur.gursoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2026-08-29 16:38:02 +0300

    bug fix

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+
+@Global()
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
